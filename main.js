726ffc2af2d4bd2ba83365debd0ebab24c6bd420
Farhan-Farid <farhanfaridkhan2024@gmail.com>
2025-10-13 17:24:59 +0600

    commit 4

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let nameshow = document.getElementById
+const nameshow = document.getElementById('nameparam');
+const mailshow = document.getElementById('mailparam');
 
+mailshow.innerHTML = JSON.parse(localStorage.getItem("userData")).email;
+nameshow.innerHTML = JSON.parse(localStorage.getItem("userData")).name;
 
+const profileimg = document.getElementById('profimg');
+
+    profileimg.addEventListener('click', () => {
+      // Toggle the Tailwind ring or border class
+      profileimg.classList.toggle('ring-2');
+      profileimg.classList.toggle('ring-baseGreen-400');
+      profileimg.classList.toggle('ring-offset-2');
+    });
 
 
 
